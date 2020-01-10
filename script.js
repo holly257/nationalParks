36bdd58d5b14a6ee4377a7e1824e4495dddf0d1e
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-01-09 21:41:20 -0500

    fixed statesList

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
 'use strict';
-const allStates = [ 'AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY' ];
 
 const apiKey = "iKbOKYcWmcGn6Kw5VkZnsqFby54WGsrnvDs0Qefu"
 const endPoint = "https://developer.nps.gov/api/v1/parks"
-let quantity = 10;
-
-var statesArr; 
-function validStates(statesList){
-  if(statesList.length < 1){
-    displayError("Comma delimited state list malformed. Example: AL, GA");
-    return false;
-  }
-  statesList = statesList.replace(/ /g, "").toUpperCase();
-  statesArr = statesList.split(","); 
-  
-  statesArr.forEach(st => {
-    if(st.length != 2){
-      displayError(st + " is not a valid state code.  Example: AL"); 
-      return false; 
-    }
-    if(allStates.indexOf(st) == -1){
-      displayError(st + " is not a valid state code.  Example: GA")
-    }
-  }); 
-  return true;
-}
 
 function formatParams(params) {
     const quertyItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -32,11 +9,11 @@ function formatParams(params) {
 }
 
 
-function getResults(statesList, maxResults) {
-    console.log(statesList, maxResults);
+function getResults(search, maxResults) {
+    console.log(search, maxResults);
     const params = {
         api_key : apiKey,
-        stateCode : statesArr.join(","),
+        stateCode : search,
         limit : maxResults
     };
 
@@ -76,13 +53,11 @@ function displayError(err){
 function watchForm (){
     $("form").submit (event => {
         event.preventDefault();
-        const statesList = $("#search").val();
-        quantity = $("#quantity").val();
+        const statesList = $("#search").val().replace(/ /g, "");
+        const quantity = $("#quantity").val();
         getResults(statesList, quantity);
-})
+});
 }
-
-
-    
+  
 
 $(watchForm);
