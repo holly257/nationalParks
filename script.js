b0871472a980568dff6582997ece195ab0745a53
Holly Rogers <hollyrogers@Hollys-Air.lan>
2020-01-09 20:06:20 -0500

    added url and api

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 
+const apiKey = "iKbOKYcWmcGn6Kw5VkZnsqFby54WGsrnvDs0Qefu"
+const url = "https://developer.nps.gov/api/v1"
 
+function formatParams () {
+
+}
+
+
+function getResults(query, maxResults) {
+    
+    console.log(query, maxResults);
+
+
+}
+
+function displayResults() {
+
+}
 
 function watchForm (){
     $("form").submit (event => {
         event.preventDefault();
         const search = $("#search").val();
         const quantity = $("#quantity").val();
-        console.log(search);
-        console.log(quantity);
+        getResults(search, quantity);
 })
 }
     
